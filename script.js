16620159aa38f3981fd0985e178d76aff05a590a
hassimioudiallo <hddinguiraye@gmail.com>
2023-02-14 21:39:19 -0500

    finish adding comment

--- script.js
+++ script.js
@@ -6,7 +6,6 @@ var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m"
 var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 var numberCase = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 var specialCase = ["`", "~", "!", "@", "#", "$", "%", "^", "&", "*", "=", ":", ";", "?", "/", ",", "."];
-var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
@@ -16,15 +15,31 @@ function writePassword() {
   passwordText.value = password;
 
 }
+// Create function to generatepassord
 function generatePassword() {
 
+// Create an empty array variable for characters
   var characters = []
+  
+  // Create an empty array variable for usercharacters
+  var userCharacters = []
+ 
+  // Create an empty array variable for possibleCharacters
+  var possibleCharacters = []
 
-  var length = window.prompt("how long the password to be");
+  // Create an empty array variable for the result
+  var result = [];
+
+
+  var length = parseInt(window.prompt("how long the password to be"));
   console.log(length);
-if (length < 8 || length > 128 || isNaN(length)) {
+if (length < 8 || length > 128) {
   window.alert("must be a number at least 8 and less 128");
-  return
+  return null
+}
+if (isNaN(length)) {
+  window.alert("must choose a number");
+  return null
 }
 
   var lower = window.confirm("do you want lowercase character");
@@ -37,21 +52,53 @@ if (length < 8 || length > 128 || isNaN(length)) {
   console.log(specialCharacter);
   if (!lower && !upper && !specialCharacter && !number) {
     window.alert("you must choose at least one character");
-    return 
+    return null
   }
-  if (lower){
-    characters.push(...lowerCase)
+
+  let userOptions = {
+    length: length,
+    lower: lower,
+    number: number
+    upper: upper,
+    specialCharacter: specialCharacter,
+
   }
-  if (upper){
-    characters.push(...upperCase)
+
+  if(!userOptions) return null;
+
+  if(userOptions.lower) {
+    possibleCharacters = possibleCharacters.concat(lowerCase)
+    userCharacters.push(getRand(lowerCase))
   }
-  if (number){
-    characters.push(...numberCase)
+  if(userOptions.number) {
+    possibleCharacters = possibleCharacters.concat(numberCase)
+    userCharacters.push(getRand(numberCase))
   }
-  if (specialCharacter){
-    characters.push(...specialCase)
+
+  for(let i = 0; i < userOptions.length; i++) {
+    let possibleCharacter = getRand(possibleCharacters);
+
+    result.push(possibleCharacter);
   }
-  console.log(characters)
+
+  // Create a for loop for userCharacters and join the result 
+
+  for(let i = 0; i < userCharacters.length; i++) {
+    result[i] = userCharacters[i];
+  }
+
+  return result.join("");
+
+
 }
 // Add event listener to generate button
+// var generateBtn = document.querySelector('.btn');
 generateBtn.addEventListener("click", writePassword);
+
+// function to randomize all the generated elements
+function getRand (arr) {
+  let randomizer = Math.floor(Math.random() * arr.length);
+  let randEl = arr[randomizer];
+  console.log(randEl);
+  return randEl;
+}
